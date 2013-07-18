e6c19a82d07de092fda00a4a0be8c1a2b9535bd1
Jing Dong <jing.dong@activars.com>
2013-07-18 12:36:39 +0100

    allow set cofig file group and owner

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,17 +1,27 @@
-define s3cmd::config ($s3key = '', $s3aid = '', $bucket_location = 'eu-west-1', $path = '/root/.s3cfg')  {
+define s3cmd::config (
+    $s3key = '',
+    $s3aid = '',
+    $bucket_location = 'eu-west-1',
+    $path  = '/root/.s3cfg',
+    $group = 'root',
+    $owner = 'root',
+    $mode  = '600'
+) {
     include s3cmd::params
 
     if ($s3key == '') {
-        fail('Please specify s3key to access S3:')
+        fail('Please specify s3key to access S3')
     }
     if ($s3aid == '') {
         fail('Please specify s3aid to access S3')
     }
 
-    file { "$path": 
-        ensure => present,
+    file { "$path":
+        ensure  => present,
         content => template('s3cmd/s3cmd.erb'),
-        mode => '0600',
-        require => Package[$s3cmd::params::packages];
+        group   => $group,
+        owner   => $owner,
+        mode    => $mode,
+        require => Package[$s3cmd::params::packages],
     }
 }
